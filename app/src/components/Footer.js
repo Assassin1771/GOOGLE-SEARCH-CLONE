26b90783d5a5bb99647135ca7ce053d67a9eba53
Tushar Garg <tshrgarg2010@gmail.com>
2022-01-21 22:36:13 +0530

    Changed Google styling

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Footer = () => (
   <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
-    © 2022 Goggl, Inc.
+    © 2022 Google, Inc.
   </div>
 );
--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -7,8 +7,15 @@ export const Navbar = ({ setDarkTheme, darkTheme }) => (
   <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
     <div className="flex justify-between items-center space-x-5 w-screen ">
       <Link to="/">
-        <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
-          GOOGLE
+        <p className="text-3xl font-bold py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
+          {/* <p className="text-2xl font-bold py-1 rounded bg-blue-500"> */}
+          <span className="text-blue-500">G</span>
+          <span className="text-red-600">o</span>
+          <span className="text-yellow-300">o</span>
+          <span className="text-blue-500">g</span>
+          <span className="text-green-600">l</span>
+          <span className="text-red-600">e</span>
+          {/* <span className="">GOOGLE</span> */}
         </p>
       </Link>
       <button
