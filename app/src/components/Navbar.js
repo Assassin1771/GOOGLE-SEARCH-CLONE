a04c9d6a15330c3c7cbae7a30275672ffdd62773
Tushar Garg <tshrgarg2010@gmail.com>
2022-01-21 22:39:37 +0530

    Changed dark and light icons

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -7,15 +7,13 @@ export const Navbar = ({ setDarkTheme, darkTheme }) => (
   <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
     <div className="flex justify-between items-center space-x-5 w-screen ">
       <Link to="/">
-        <p className="text-3xl font-bold py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
-          {/* <p className="text-2xl font-bold py-1 rounded bg-blue-500"> */}
+        <p className="text-3xl font-bold py-1 px-2 rounded">
           <span className="text-blue-500">G</span>
           <span className="text-red-600">o</span>
           <span className="text-yellow-300">o</span>
           <span className="text-blue-500">g</span>
           <span className="text-green-600">l</span>
           <span className="text-red-600">e</span>
-          {/* <span className="">GOOGLE</span> */}
         </p>
       </Link>
       <button
@@ -23,7 +21,7 @@ export const Navbar = ({ setDarkTheme, darkTheme }) => (
         onClick={() => setDarkTheme(!darkTheme)}
         className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg"
       >
-        {darkTheme ? "💡 Light" : "🌙 Dark"}
+        {darkTheme ? "Light 🌞" : "Dark 🌙"}
       </button>
     </div>
     <Search />
