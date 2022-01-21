fcee7b2f8dad87abecc9cf3f22d9c0b76e006d69
Tushar Garg <tshrgarg2010@gmail.com>
2022-01-21 22:54:39 +0530

    Changed loading icon and text and set default theme to dark

--- app/src/components/Loading.js
+++ app/src/components/Loading.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import Loader from 'react-loader-spinner';
+import React from "react";
+import Loader from "react-loader-spinner";
+import { Loading as Dots } from "react-loading-dot";
 
 export const Loading = () => (
-  <div className="flex justify-center items-center ">
-    <Loader type="Puff" color="#00BFFF" height={550} width={80} />
-  </div>
+  <>
+    <div className="flex justify-center items-center ">
+      <Loader type="Oval" color="#00BFFF" height={30} width={30} />
+    </div>
+    <div>
+      <p className="text-blue-500 text-xl m-5 font-bold text-center">
+        LOADING . . .
+      </p>
+    </div>
+  </>
 );
